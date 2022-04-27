88b52022c75383a6ece81ca273ec3c0f383defb3
DakotaRileyMedia <dakotacustompcs@gmail.com>
2022-04-27 16:50:54 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dakota Riley on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
